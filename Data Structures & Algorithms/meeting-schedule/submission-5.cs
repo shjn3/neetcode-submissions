2521a766197f4f78662041c9e5f01ebeb675f1ee
shjn3 <70001950+shjn3@users.noreply.github.com>
2026-07-14 21:44:24 +0700

    Add: meeting-schedule - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule/submission-5.cs
@@ -0,0 +1,22 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool CanAttendMeetings(List<Interval> intervals) {
+        intervals.Sort((a,b)=>a.start.CompareTo(b.start));
+
+        for(int i =1;i<intervals.Count;i++){
+            if(intervals[i].start<intervals[i-1].end) return false;
+        }
+
+        return true;
+    }
+}
