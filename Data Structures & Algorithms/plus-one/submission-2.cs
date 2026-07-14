e1b4d0591d05507135e40145180773084771cd75
shjn3 <70001950+shjn3@users.noreply.github.com>
2026-07-14 22:08:06 +0700

    Add: plus-one - submission-2

---
+++ Data Structures & Algorithms/plus-one/submission-2.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        int remainVal = 1;
+        bool isUseNewArr = digits[0]==9;
+        int[] newDigits = new int[isUseNewArr?digits.Length+1:0];
+        for(int i =digits.Length-1;i>=0;i--){
+            int nextVal = remainVal+digits[i];
+            if(nextVal>=10){
+                digits[i]=0;
+                remainVal = nextVal/10;
+            }else{
+                remainVal =0;
+                digits[i]=nextVal;
+                if(isUseNewArr) newDigits[i+1]=nextVal;
+                break;
+            }
+        }
+        if(remainVal==0) return digits;
+
+        newDigits[0]=remainVal;
+        return newDigits;
+    }
+}
