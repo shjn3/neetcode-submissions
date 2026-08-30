54931ffe1854248dc00b17a0340d446a2804d14e
shjn3 <70001950+shjn3@users.noreply.github.com>
2026-08-30 13:26:24 +0700

    Add: longest-increasing-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-3.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int LengthOfLIS(int[] nums) {
+        int n =nums.Length;
+        int[] dp = new int[n];
+        int max =0;
+        for(int i =0;i<n;i++){
+            for(int j =i+1;j<n;j++){
+                if(nums[j]>nums[i]){
+                    dp[j] = Math.Max(dp[j],dp[i]+1);
+                    max = Math.Max(dp[j],max);
+                }
+            }
+        }
+
+        return max+1;
+    }
+}
