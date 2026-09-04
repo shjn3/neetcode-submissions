518536f78278f966621df1032e68522364b79473
shjn3 <70001950+shjn3@users.noreply.github.com>
2026-09-04 11:31:00 +0700

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int UniquePaths(int m, int n) {
+        int[,] dp = new int[m,n];
+
+        for(int i=0;i<n;i++){
+            dp[m-1,i]=1;
+        }
+        for(int j=0;j<m;j++){
+            dp[j,n-1]=1;
+        }
+
+
+        dp[m-1,n-1]=1;
+
+        for(int i =m-2;i>=0;i--){
+            for(int j = n-2;j>=0;j--){
+                dp[i,j] = dp[i+1,j]+dp[i,j+1];
+            }
+        }
+
+        return dp[0,0];
+    }
+}
